f6604df6b4f2cdda01834b94a51f8e5cafb1a961
patanfouziya <138987303+patanfouziya@users.noreply.github.com>
2025-02-13 16:44:32 +0530

    Update Abc.java

--- Abc.java
+++ Abc.java
@@ -1,5 +1,7 @@
-public class Welcome {
-    public static void main(String[] args) {
+public class Abc
+{
+    public static void main(String[] args) 
+    {
         System. out.println("Welcome to Java world..!");
     }
 }
